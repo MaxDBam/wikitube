c0281c0ed84c871afdb46e16267da51bcbd65d8b
Max <mdubovsky@gmail.com>
2021-12-04 13:48:44 +0200

    Added bonuses

--- js/main-controller.js
+++ js/main-controller.js
@@ -12,6 +12,7 @@ function onInit() {
 
 function onGetYoutubeAndWikiQueries() {
     let elInput = document.querySelector('input[type="search"')
+    saveTerms(elInput.value)
     getYoutubeVideoAndList(elInput.value)
         .then(renderVideoAndList)
         .catch(error => console.log(error))
@@ -19,6 +20,55 @@ function onGetYoutubeAndWikiQueries() {
     getWikiQuery(elInput.value)
         .then(renderWikiInfo)
         .catch(error => console.log(error))
+    renderSearchedTerms()
+    elInput.value = ' '
+}
 
-    elInput.value = ''
+function renderSearchedTerms() {
+    let elSearchedTermsP = document.querySelector('.searched-terms')
+    elSearchedTermsP.innerText = getTerms()
+}
+
+function onRemoveHistory() {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will lose the searched terms history.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                clearTerms()
+                renderSearchedTerms()
+                swal("Poof! Your searched terms were deleted!", {
+                    icon: "success",
+                })
+            } else {
+                swal("Your searched terms are safe")
+            }
+        })
+}
+
+function onChangeTheme() {
+    const colorPicker = document.createElement('input')
+    colorPicker.type = 'color'
+    swal({
+        title: "Are you sure?",
+        text: "The background color will be changed.",
+        icon: "warning",
+        content: colorPicker,
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                document.querySelector('body').style.backgroundColor = colorPicker.value
+                swal("Poof! the background color of the page has changed", {
+                    icon: "success",
+                })
+            } else {
+                swal("The background color was not changed.")
+            }
+        })
 }
