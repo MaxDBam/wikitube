8c5d9b41d13a2e26aa8cf474be36dfc9c1e19f56
MaxDBam <62358630+MaxDBam@users.noreply.github.com>
2021-12-05 21:05:15 +0200

    Fixed storage

---
+++ js/main-service.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const STORAGE_KEY = 'terms'
+var gTerms;
+
+function saveTerms(term) {
+    gTerms.push(term)
+    saveToStorage(STORAGE_KEY, gTerms)
+}
+
+function getTerms() {
+    let searchedTerms = LoadFromStorage(STORAGE_KEY)
+    if (!searchedTerms || !searchedTerms.length) {
+        searchedTerms = []
+    } 
+
+    gTerms = searchedTerms
+    // saveTerms()
+
+    return gTerms.join(', ')
+}
+
+function clearTerms() {
+    gTerms = []
+    localStorage.clear()
+}
